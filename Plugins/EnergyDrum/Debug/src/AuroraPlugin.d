fe8850b14562b0349591e58248a6ac640ed06111
yanxi1 <xi.yan@mail.utoronto.ca>
2017-07-24 13:48:21 -0400

    final demo

---
+++ Plugins/EnergyDrum/Debug/src/AuroraPlugin.d
@@ -0,0 +1,22 @@
+src/AuroraPlugin.o: ../src/AuroraPlugin.cpp ../inc/AuroraPlugin.h \
+  ../inc/LayoutProcessingUtils.h ../inc/Point.h ../inc/Shape.h \
+  ../inc/ColorUtils.h ../inc/DataManager.h ../inc/PluginFeatures.h \
+  ../inc/Logger.h ../inc/SoundUtils.h
+
+../inc/AuroraPlugin.h:
+
+../inc/LayoutProcessingUtils.h:
+
+../inc/Point.h:
+
+../inc/Shape.h:
+
+../inc/ColorUtils.h:
+
+../inc/DataManager.h:
+
+../inc/PluginFeatures.h:
+
+../inc/Logger.h:
+
+../inc/SoundUtils.h:
